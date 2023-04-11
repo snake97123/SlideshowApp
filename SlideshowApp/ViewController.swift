0e9af274c5a01ef9f8fdce176c3ab7ba855e7d65
snake97123 <snake12379@yahoo.co.jp>
2023-04-11 16:30:43 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Wataru Yamashita on 2023/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
