d49c70c3fab8e6fb677d6c818f22e51c82ba41eb
snake97123 <snake12379@yahoo.co.jp>
2023-04-12 18:33:54 +0900

    スライドショーアプリの作成をした。

---
+++ SlideshowApp/CloseUpImageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CloseUpImageViewController.swift
+//  SlideshowApp
+//
+//  Created by Wataru Yamashita on 2023/04/11.
+//
+
+import UIKit
+
+class CloseUpImageViewController: UIViewController {
+
+    @IBOutlet weak var closeUpImageView: UIImageView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var flowerImage: UIImageView!
+    var imageIndex = 0
+    var timer: Timer?
+    let images = ["flower1.jpeg", "flower2.jpeg", "flower3.jpeg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateImage()
+        
+    }
+    
+    @objc func updateImage() {
+        imageIndex += 1
+        if imageIndex >= images.count {
+            imageIndex = 0
+        }
+        flowerImage.image = UIImage(named: images[imageIndex])
     }
 
-
+    @IBAction func forwardButtonTapped(_ sender: Any) {
+        updateImage()
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        imageIndex -= 1
+        if imageIndex < 0 {
+            imageIndex = images.count - 1
+        }
+        flowerImage.image = UIImage(named: images[imageIndex])
+    }
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true)
+            playButton.setTitle("停止", for: .normal)
+            forwardButton.isEnabled = false
+            backButton.isEnabled = false
+        } else {
+            timer?.invalidate()
+            self.timer = nil
+            playButton.setTitle("再生", for: .normal)
+            forwardButton.isEnabled = true
+            backButton.isEnabled = true
+        }
+    }
+    @IBAction func tapImage(_ sender: Any) {
+      
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let closeUpImageViewController: CloseUpImageViewController = segue.destination as! CloseUpImageViewController
+        closeUpImageViewController.loadViewIfNeeded()
+        closeUpImageViewController.closeUpImageView.image = UIImage(named: images[imageIndex])
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 }
 
